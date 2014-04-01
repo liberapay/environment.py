c2fb56e9e147678a177fca8a9cf3d078ff341b4d
Chad Whitacre <chad@zetaweb.com>
2014-04-01 12:34:05 -0400

    Finish with first pass on docs and tests

--- environment.py
+++ environment.py
@@ -23,32 +23,54 @@ Tutorial
 
 First let's pretend that this is our ``os.environ``:
 
->>> pretend_os_environ = {'FOO': '42', 'BAR_BAZ': 'buz', 'BAR_BLOO': 'yes'}
+>>> pretend_os_environ = { 'FOO': '42'
+...                      , 'BAR_BAZ': 'buz'
+...                      , 'BAR_BLOO_BLOO': 'yes'
+...                      , 'BAD': 'to the bone'
+...                       }
 
-The way this library works is you instantiate an :py:class:`Environment` class
-like so:
+The way the :py:mod:`environment` library works is you instantiate an
+:py:class:`Environment` class like so:
 
 >>> from environment import Environment, is_yesish
->>> env = Environment( 'FOO', 'BAR_'
-...                  , FOO=int, BAR_BLOO=is_yesish
-...                  , environ=pretend_os_environ
+>>> env = Environment( FOO=int
+...                  , BAR_BAZ=None
+...                  , BAR_BLOO_BLOO=is_yesish
+...                  , BLAH=None
+...                  , BAD=int
+...                  , _environ=pretend_os_environ
 ...                   )
+
+Keyword arguments (besides ``_environ``, which is special-cased) specify which
+variables to look for and how to typecast them. A type of ``None`` will
+result in a value of type :py:class:`str` (:py:class:`unicode` under Python 2).
+Since a process environment contains a lot of crap you don't care about, we
+only parse out variables that you explicitly specify in the keyword arguments.
+
+The resulting object has lowercase attributes for all variables that were asked
+for and found:
+
 >>> env.foo
 42
+
+We support one level of namespacing:
+
 >>> env.bar.baz
 'buz'
->>> env.bar.bloo
+>>> env.bar.bloo_bloo
 True
 
-Args are whitelisted environment variable names. If you tack on underscore then
-any environment variables beginning with ``BAR_`` will be allowed, and they'll
-be stored on a subobject.
+There are also :py:attr:`~Environment.missing` and
+:py:attr:`~Environment.malformed` attributes for variables that weren't found
+or couldn't be typecast:
 
-Keyword args are typecasters for each variable (``environ`` is special-cased to
-allow you to override the default, which is of course to use ``os.environ``).
+>>> env.missing
+['BLAH']
+>>> env.malformed
+{'BAD': "ValueError: invalid literal for int() with base 10: 'to the bone'"}
 
-So here's what the above produces:
-
+You're expected to inspect the contents of :py:attr:`~Environment.missing` and
+:py:attr:`~Environment.malformed` and do your own error reporting.
 
 
 API Reference
@@ -64,17 +86,42 @@ import sys
 __version__ = '0.0.0-dev'
 
 
-class Section(object):
-    pass
-
-
 class Environment(object):
-    """This class represents a process environment.
+    """This class represents a subset of a process environment.
+
+    :param spec: Keyword arguments are of the form ``VARIABLE_NAME=type``. If
+        ``type`` is None then the value will be stored as :py:class:`str`
+        (:py:class:`unicode` under Python 2).
+
+    :param mapping _environ: By default of course we look at
+        :py:attr:`os.environ`, but you can override that with this, which can
+        only be given as a keyword argument. We operate on a shallow copy of this
+        mapping (though if all values are strings, it's effectively a deep copy,
+        since strings are immutable).
+
+    The constructor for this class loops through the items in ``_environ``,
+    skipping those variables not also named in ``spec``, and parsing those that
+    are. We store variables using lowercased names.
+
+    If a variable is mentioned in ``spec`` but is not in ``_environ``, the
+    variable name is recorded in the :py:attr:`missing` list. If typecasting a
+    variable raises an exception, the variable name and an error message are
+    recorded in the :py:attr:`malformed` dictionary.
+
+    If a variable name includes an underscore (``_``), then the first part of
+    the name is taken to be a namespace, and all variables beginning with that
+    part are collected under an :py:class:`EnvironmentVariableGroup` instance.
+    The rest of the name is the attribute name. So ``MYAPP_VARIABLE_NAME``
+    would end up at ``env.myapp.variable_name``. We only support one level of
+    namespacing, and malformed variables don't generate namespaces.
 
     """
 
+    missing = []    #: A list of variable names that are in ``spec`` but not ``_environ``.
+    malformed = {}  #: A dictionary of error messages for typecasting failures, keyed by variable name.
+
     def __init__(self, **spec):
-        environ = spec.pop('environ', os.environ)
+        environ = spec.pop('_environ', os.environ)
 
         # We're going to mutate environ, so let's work with a copy. It can be a
         # shallow copy since the values are all strings (which are immutable, so
@@ -86,44 +133,61 @@ class Environment(object):
 
         for name, value in sorted(environ.items()):
 
+            # Skip envvars we don't care about.
             if name not in spec:
                 continue
-
-            parts = name.split('_')
-            first = parts[0]
-
-            # Pick an object and attribute name.
-            obj, attr = self, name
-            if len(parts) > 1:
-                section = first.lower()
-                if section not in self.__dict__:
-                    # Create a new section if need be.
-                    self.__dict__[section] = Section()
-                obj = self.__dict__[section]
-                attr = '_'.join(parts[1:])
-            attr = attr.lower()
-
-            # Decide how to typecast.
-            cast = spec.get(name)
-            if cast is None:
-                cast = lambda o: o
 
             # Ensure we have a string.
             if sys.version_info < (3, 0, 0):
                 encoding = sys.getfilesystemencoding()
                 value = value.decode(encoding)
 
+            # Decide how to typecast.
+            type_ = spec[name]
+            if type_ is None:
+                type_ = lambda o: o
+
+            # Typecast!
             try:
-                value = cast(value)
+                value = type_(value)
             except:
                 exc_type, exc_instance = sys.exc_info()[:2]
                 msg = "{}: {}".format(exc_type.__name__, exc_instance)
                 self.malformed[name] = msg
-            else:
-                obj.__dict__[attr] = value
+                continue
+
+            # Pick an object and attribute name.
+            parts = name.split('_')
+            first = parts[0]
+            obj = self
+            attr = name
+            if len(parts) > 1:
+                section = first.lower()
+                if section not in self.__dict__:
+                    # Create a new section if need be.
+                    self.__dict__[section] = EnvironmentVariableGroup()
+                obj = self.__dict__[section]
+                attr = '_'.join(parts[1:])
+            attr = attr.lower()
+
+            # Store the value.
+            obj.__dict__[attr] = value
+
+
+class EnvironmentVariableGroup(object):
+    """Instantiated by :py:class:`Environment` to represent groups of environment variables.
+    """
 
 
 def is_yesish(value):
+    """Typecase booleanish environment variables to :py:class:`bool`.
+
+    :param string value: An environment variable value.
+
+    :return bool: Return :py:class:`True` if ``value`` is ``1``, ``true``, or
+        ``yes`` (case-insensitive), and :py:class:`False` otherwise.
+
+    """
     return value.lower() in ('1', 'true', 'yes')
 
 
--- tests.py
+++ tests.py
@@ -5,46 +5,50 @@ from environment import Environment, is_yesish
 
 
 def test_Environment_basically_works():
-    env = Environment(FOO_BAR=None, environ={'FOO_BAR': 'baz'})
+    env = Environment(FOO_BAR=None, _environ={'FOO_BAR': 'baz'})
     assert env.foo.bar == 'baz'
     assert env.missing == []
     assert env.malformed == {}
 
 def test_Environment_unprefixed_works():
-    env = Environment(FOO=None, environ={'FOO': 'baz'})
+    env = Environment(FOO=None, _environ={'FOO': 'baz'})
     assert env.foo == 'baz'
 
 def test_Environment_missing_is_missing():
-    env = Environment(FOO=None, environ={})
+    env = Environment(FOO=None, _environ={})
     assert env.missing == ['FOO']
 
 def test_Environment_malformed_is_malformed():
-    env = Environment(FOO=int, environ={'FOO': 'baz'})
+    env = Environment(FOO=int, _environ={'FOO': 'baz'})
     assert env.malformed == {'FOO': "ValueError: invalid literal for int() with base 10: 'baz'"}
 
 def test_Environment_extra_is_ignored():
-    env = Environment(FOO=None, environ={'FOO_BAR_BAZ': '42'})
+    env = Environment(FOO=None, _environ={'FOO_BAR_BAZ': '42'})
     assert env.missing == ['FOO']
     assert sorted(env.__dict__.keys()) == ['malformed', 'missing']
 
 def test_Environment_typecasting_works():
-    env = Environment(FOO=int, environ={'FOO': '42'})
+    env = Environment(FOO=int, _environ={'FOO': '42'})
     assert env.foo == 42
 
 def test_Environment_complex_typecasting_works():
     class MyType(object):
         def __init__(self, val):
             self.val = 'cheese'
-    env = Environment(FOO=MyType, environ={'FOO': '42'})
+    env = Environment(FOO=MyType, _environ={'FOO': '42'})
     assert env.foo.val == 'cheese'
 
 def test_Environment_namespacing_works():
-    env = Environment(FOO_BAR=None, environ={'FOO_BAR': '42'})
+    env = Environment(FOO_BAR=None, _environ={'FOO_BAR': '42'})
     assert env.foo.bar == '42'
 
 def test_Environment_namespacing_works_but_only_one_level():
-    env = Environment(FOO_BAR_BAZ=int, environ={'FOO_BAR_BAZ': '42'})
+    env = Environment(FOO_BAR_BAZ=int, _environ={'FOO_BAR_BAZ': '42'})
     assert env.foo.bar_baz == 42
+
+def test_Environment_malformed_values_dont_generate_namespaces():
+    env = Environment(FOO_BAR_BAZ=int, _environ={'FOO_BAR_BAZ': 'blah'})
+    assert 'foo' not in env.__dict__
 
 
 def test_is_yesish_1_is_True():
